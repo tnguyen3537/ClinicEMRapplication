c752105d8803da0f31f1223c0c70b960565e9fba
Toan Nguyen <tnguyen3537@gmail.com>
2024-04-14 17:53:26 -0500

    First Commit Blank App

---
+++ src/main/java/clinic/emr/clinicemr/ClinicemrApplication.java
@@ -0,0 +1,13 @@
+package clinic.emr.clinicemr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicemrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicemrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clinic/emr/clinicemr/ClinicemrApplicationTests.java
@@ -0,0 +1,13 @@
+package clinic.emr.clinicemr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicemrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
